0a27d436291506bb24eaead188e306d31eb1ea3f
coda1997 <overlooker1997@gmail.com>
2021-07-01 23:51:54 +0800

    use async code to load json and fetch images

--- AnimeTimeLine/ContentView.swift
+++ AnimeTimeLine/ContentView.swift
@@ -8,22 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    let imageUrl = URL(string: "https://bangumi.moe/data/images/2014/12/6dznn2rykghikpgav2k.jpg")
+    let baseUrlString = "https://bangumi.moe/"
+    @StateObject var animes = Animes()
     var body: some View {
-        if #available(iOS 15.0, *) {
-            AsyncImage(url: imageUrl){image in
-                image.resizable()
-                    .scaledToFit()
-            } placeholder: {
-                ProgressView()
-                
+        List{
+            ForEach(animes.items){ anime in
+                AsyncImage(url: URL(string: baseUrlString+anime.cover)){image in
+                    image.resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
             }
-                
-        } else {
-            // Fallback on earlier versions
+            
+        }.task {
+            await animes.loadData()
+            animes.items.sort(by: {
+                $0.startDate > $1.startDate
+            })
+        }
+ 
+    }
+    
+
+}
+
+class Animes: ObservableObject {
+    @Published var items: [Anime] = []
+    
+    fileprivate func loadData() async{
+        guard let url = URL(string: "https://bangumi.moe/api/bangumi/current") else {
+            return
+        }
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let animes = try JSONDecoder().decode([Anime].self, from: data)
+            items = animes
+        }
+        catch{
+            return
         }
     }
 }
+
+struct Anime: Codable {
+    var id: String{
+        _id
+    }
+    var _id: String
+    var name: String
+    var credit: String
+    var startDate: Int
+    var endDate: Int
+    var icon: String
+    var cover: String
+}
+extension Anime: Identifiable{}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
