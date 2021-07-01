3764edb54988ff36f301708fd38eb40ccf60b5d1
coda1997 <overlooker1997@gmail.com>
2021-07-01 22:41:39 +0800

    new feature: asyncImageView

--- AnimeTimeLine/ContentView.swift
+++ AnimeTimeLine/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let imageUrl = URL(string: "https://bangumi.moe/data/images/2014/12/6dznn2rykghikpgav2k.jpg")
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if #available(iOS 15.0, *) {
+            AsyncImage(url: imageUrl){image in
+                image.resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+                
+            }
+                
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
 
