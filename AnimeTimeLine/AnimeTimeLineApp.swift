e473d50bfbc31a26009ea73dc149c9f5c0ab6d02
coda1997 <overlooker1997@gmail.com>
2021-07-01 22:11:59 +0800

    Initial Commit

---
+++ AnimeTimeLine/AnimeTimeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTimeLineApp.swift
+//  AnimeTimeLine
+//
+//  Created by 沈达 on 2021/7/1.
+//
+
+import SwiftUI
+
+@main
+struct AnimeTimeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
